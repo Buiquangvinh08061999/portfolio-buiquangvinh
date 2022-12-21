08814cf5f71148f8f25e52caf92cea2be84e73b6
vinh <top.xom.ray@gmail.com>
2022-12-21 10:34:52 +0700

    Update portfolio

--- js/main.js
+++ js/main.js
@@ -1,21 +1,25 @@
 (function ($) {
     "use strict";
 
+
+  
     // Spinner
     var spinner = function () {
+
         setTimeout(function () {
             if ($('#spinner').length > 0) {
                 $('#spinner').removeClass('show');
             }
         }, 1);
+
     };
     spinner();
 
    
     // Initiate the wowjs
     new WOW().init();
-
-
+    
+    
     // Navbar on scrolling
     $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
